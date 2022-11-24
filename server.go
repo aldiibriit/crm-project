d2aa0790e853b5317b5b653ad776d8e226c3125d
aldi <aldi.sptra86@gmail.com>
2022-11-24 13:22:09 +0700

    add response salesPhone at misDeveloper

--- server.go
+++ server.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"encoding/base64"
-	"fmt"
 	"go-api/config"
 	"go-api/controller"
-	"go-api/helper"
+	"go-api/middleware"
 	"go-api/repository"
 	"go-api/service"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
@@ -40,70 +39,70 @@ var (
 	salesController        controller.SalesController        = controller.NewSalesController(salesService)
 )
 
-func main() {
-	// Password := "feryganteng@yopmail.com"
-	// encryptedPassword, _ := helper.RsaEncryptFEToBE([]byte(Password))
-	// fmt.Println("Password : ", encryptedPassword)
-	decodedNama, _ := base64.StdEncoding.DecodeString("fmDO1p4jofEp1vXNKJHJinv/UwLNT7JCrfMWFVMkWYiGhScWJdHLD6LwTkLQiYIJj13dQKAoiWwhL6XLvBzO38rkKmYy5LiGMtLezkAlmFL0YADqEsjY6xAzOZw8j58jnTsrN6ZSTaUjc9jJLVxOj3yHBKORtP1k2A7R2x46J22LqBhDsFBwijb/m5iCBCgOWyVMLHHhdqJlMTCo4cVduL6los6T1Elfmqew3ko8USPKfB+C9DSVzmtdBaJy+FyLnwf0cp9y57mgTcHjBatsCCX9/uYtcZAB3hRzML7d3jM4aVpTvJttPLE37cFq+Kl/gnbH55HSIDSp+GTdMA/u7IEvXWo7CBst1ciDfRxaCh/Rz/ax0PLSx1LxeBhyYiMPjOQ6Erj8ZZ4Gzf8APVJjsjqz4cdte5hFZ8q04Az6I760LyOHN6dgXRy7mE9GOl+gKWRN9pWRb4c+1T2ZX54D+gwITORF3rzDhOGcac9o75nYKFbcNI+uLepDDqYt8/2tnlKh2Qt77beq06+YbuygjIOmw54jkkpxgCiiwFdlg8tnkrudnO+UyXhQOF615i41XhdsCs7hl9aufcqGkzyVVuIsyP69bhYifweL1udUMrIEsHmxhAugIK94tH1kcdEgI2LFYRqSKA+uyWVV65r1B3aca2ds9SHPt/InmIuYnMM=")
-	plainNama, _ := helper.RsaDecryptFromBEInFE(decodedNama)
-	fmt.Println(plainNama)
-	// id := uuid.New()
-	// fmt.Println(reflect.TypeOf(id))
-	// fmt.Println(reflect.TypeOf(id.String()))
-}
-
 // func main() {
-// 	defer config.CloseDatabaseConnection(db)
-// 	r := gin.Default()
-
-// 	r.Use(middleware.CORSMiddleware())
-
-// 	authRoutes := r.Group("api/auth")
-// 	{
-// 		authRoutes.POST("/login", authController.Login)
-// 		authRoutes.POST("/register", authController.Register)
-// 		authRoutes.POST("/createToken", authController.CreateToken)
-// 		authRoutes.POST("/registerUserSales", authController.RegisterSales)
-// 		authRoutes.POST("/activateUser", authController.ActivateUser)
-// 		authRoutes.POST("/passwordConfirmation", authController.PasswordConfirmation)
-// 	}
-
-// 	otpRoutes := r.Group("api/otp")
-// 	{
-// 		otpRoutes.POST("/validateOTP", otpController.ValidateOTP)
-// 	}
-
-// 	salesRoutes := r.Group("api/salesMIS")
-// 	{
-// 		salesRoutes.POST("/atDeveloper", salesController.MISDeveloper)
-// 		salesRoutes.POST("/atSuperAdmin", salesController.MISSuperAdmin)
-// 	}
-
-// 	userRoutes := r.Group("api/user", middleware.AuthorizeJWT(jwtService))
-// 	{
-// 		userRoutes.GET("/profile", userController.Profile)
-// 		userRoutes.PUT("/profile", userController.Update)
-// 	}
-
-// 	bookRoutes := r.Group("api/books", middleware.AuthorizeJWT(jwtService))
-// 	{
-// 		bookRoutes.GET("/", bookController.All)
-// 		bookRoutes.POST("/", bookController.Insert)
-// 		bookRoutes.GET("/:id", bookController.FindByID)
-// 		bookRoutes.PUT("/:id", bookController.Update)
-// 		bookRoutes.DELETE("/:id", bookController.Delete)
-// 	}
-
-// 	propertiRoutes := r.Group("api/properti")
-// 	{
-// 		propertiRoutes.POST("/advancedFilter", propertiController.AdvancedFilter)
-// 		propertiRoutes.POST("/landingPage", propertiController.LandingPage)
-// 	}
-
-// 	testRoutes := r.Group("/test")
-// 	{
-// 		testRoutes.POST("", testController.TestEmail)
-// 	}
-
-// 	r.Run(":7177")
+// 	// Password := "feryganteng@yopmail.com"
+// 	// encryptedPassword, _ := helper.RsaEncryptFEToBE([]byte(Password))
+// 	// fmt.Println("Password : ", encryptedPassword)
+// 	decodedNama, _ := base64.StdEncoding.DecodeString("fmDO1p4jofEp1vXNKJHJinv/UwLNT7JCrfMWFVMkWYiGhScWJdHLD6LwTkLQiYIJj13dQKAoiWwhL6XLvBzO38rkKmYy5LiGMtLezkAlmFL0YADqEsjY6xAzOZw8j58jnTsrN6ZSTaUjc9jJLVxOj3yHBKORtP1k2A7R2x46J22LqBhDsFBwijb/m5iCBCgOWyVMLHHhdqJlMTCo4cVduL6los6T1Elfmqew3ko8USPKfB+C9DSVzmtdBaJy+FyLnwf0cp9y57mgTcHjBatsCCX9/uYtcZAB3hRzML7d3jM4aVpTvJttPLE37cFq+Kl/gnbH55HSIDSp+GTdMA/u7IEvXWo7CBst1ciDfRxaCh/Rz/ax0PLSx1LxeBhyYiMPjOQ6Erj8ZZ4Gzf8APVJjsjqz4cdte5hFZ8q04Az6I760LyOHN6dgXRy7mE9GOl+gKWRN9pWRb4c+1T2ZX54D+gwITORF3rzDhOGcac9o75nYKFbcNI+uLepDDqYt8/2tnlKh2Qt77beq06+YbuygjIOmw54jkkpxgCiiwFdlg8tnkrudnO+UyXhQOF615i41XhdsCs7hl9aufcqGkzyVVuIsyP69bhYifweL1udUMrIEsHmxhAugIK94tH1kcdEgI2LFYRqSKA+uyWVV65r1B3aca2ds9SHPt/InmIuYnMM=")
+// 	plainNama, _ := helper.RsaDecryptFromBEInFE(decodedNama)
+// 	fmt.Println(plainNama)
+// 	// id := uuid.New()
+// 	// fmt.Println(reflect.TypeOf(id))
+// 	// fmt.Println(reflect.TypeOf(id.String()))
 // }
+
+func main() {
+	defer config.CloseDatabaseConnection(db)
+	r := gin.Default()
+
+	r.Use(middleware.CORSMiddleware())
+
+	authRoutes := r.Group("api/auth")
+	{
+		authRoutes.POST("/login", authController.Login)
+		authRoutes.POST("/register", authController.Register)
+		authRoutes.POST("/createToken", authController.CreateToken)
+		authRoutes.POST("/registerUserSales", authController.RegisterSales)
+		authRoutes.POST("/activateUser", authController.ActivateUser)
+		authRoutes.POST("/passwordConfirmation", authController.PasswordConfirmation)
+	}
+
+	otpRoutes := r.Group("api/otp")
+	{
+		otpRoutes.POST("/validateOTP", otpController.ValidateOTP)
+	}
+
+	salesRoutes := r.Group("api/salesMIS")
+	{
+		salesRoutes.POST("/atDeveloper", salesController.MISDeveloper)
+		salesRoutes.POST("/atSuperAdmin", salesController.MISSuperAdmin)
+	}
+
+	userRoutes := r.Group("api/user", middleware.AuthorizeJWT(jwtService))
+	{
+		userRoutes.GET("/profile", userController.Profile)
+		userRoutes.PUT("/profile", userController.Update)
+	}
+
+	bookRoutes := r.Group("api/books", middleware.AuthorizeJWT(jwtService))
+	{
+		bookRoutes.GET("/", bookController.All)
+		bookRoutes.POST("/", bookController.Insert)
+		bookRoutes.GET("/:id", bookController.FindByID)
+		bookRoutes.PUT("/:id", bookController.Update)
+		bookRoutes.DELETE("/:id", bookController.Delete)
+	}
+
+	propertiRoutes := r.Group("api/properti")
+	{
+		propertiRoutes.POST("/advancedFilter", propertiController.AdvancedFilter)
+		propertiRoutes.POST("/landingPage", propertiController.LandingPage)
+	}
+
+	testRoutes := r.Group("/test")
+	{
+		testRoutes.POST("", testController.TestEmail)
+	}
+
+	r.Run(":7177")
+}
