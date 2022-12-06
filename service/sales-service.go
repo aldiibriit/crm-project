8031e920c69865b12257ed8280537a58c142163d
aldi <aldi.sptra86@gmail.com>
2022-12-06 11:39:28 +0700

    fix endpoint

--- service/sales-service.go
+++ service/sales-service.go
@@ -36,31 +36,7 @@ func (service *salesService) MISDeveloper(request salesRequestDTO.MISDeveloperRe
 	var response responseDTO.Response
 	var metadataResponse responseDTO.ListUserDtoRes
 
-	sqlStr1 := `SELECT 
-	tu.id,ts.developer_email,ts.sales_email,ts.refferal_code,ts.registered_by,ts.created_at,ts.modified_at,ts.sales_name,tu.mobile_no as salesPhone,tu.status
-	FROM tbl_sales ts
-	JOIN tbl_user tu ON tu.email = ts.sales_email
-	WHERE developer_email = '` + request.EmailDeveloper + `' and ts.developer_email like '%` + request.Keyword + `%'
-	or developer_email = '` + request.EmailDeveloper + `' and ts.refferal_code like '%` + request.Keyword + `%'
-	or developer_email = '` + request.EmailDeveloper + `' and ts.registered_by like '%` + request.Keyword + `%'
-	or developer_email = '` + request.EmailDeveloper + `' and ts.created_at like '%` + request.Keyword + `%'
-	or developer_email = '` + request.EmailDeveloper + `' and ts.modified_at like '%` + request.Keyword + `%'
-	or developer_email = '` + request.EmailDeveloper + `' and ts.sales_name like '%` + request.Keyword + `%'
-	or developer_email = '` + request.EmailDeveloper + `' and tu.mobile_no like '%` + request.Keyword + `%'
-	limit ` + strconv.Itoa(request.Limit) + ` offset ` + strconv.Itoa(request.Offset) + `
-	`
-	sqlStrCountAll1 := `SELECT 
-	count(tu.id)
-	FROM tbl_sales ts
-	JOIN tbl_user tu ON tu.email = ts.sales_email
-	WHERE developer_email = '` + request.EmailDeveloper + `' and ts.developer_email like '%` + request.Keyword + `%'
-	or developer_email = '` + request.EmailDeveloper + `' and ts.refferal_code like '%` + request.Keyword + `%'
-	or developer_email = '` + request.EmailDeveloper + `' and ts.registered_by like '%` + request.Keyword + `%'
-	or developer_email = '` + request.EmailDeveloper + `' and ts.created_at like '%` + request.Keyword + `%'
-	or developer_email = '` + request.EmailDeveloper + `' and ts.modified_at like '%` + request.Keyword + `%'
-	or developer_email = '` + request.EmailDeveloper + `' and ts.sales_name like '%` + request.Keyword + `%'
-	or developer_email = '` + request.EmailDeveloper + `' and tu.mobile_no like '%` + request.Keyword + `%'
-	`
+	var sqlStr1, sqlStrCountAll1 string
 
 	if request.StartDate != "" && request.EndDate != "" {
 		sqlStr1 = `SELECT 
@@ -77,7 +53,7 @@ func (service *salesService) MISDeveloper(request salesRequestDTO.MISDeveloperRe
 	JOIN tbl_user tu ON tu.email = ts.sales_email
 	WHERE developer_email = '` + request.EmailDeveloper + `' and ts.created_at between '` + request.StartDate + `' and '` + request.EndDate + `'
 	`
-	} else if request.EndDate == "" {
+	} else if request.EndDate == "" && request.StartDate != "" {
 		sqlStr1 = `SELECT 
 	tu.id,ts.developer_email,ts.sales_email,ts.refferal_code,ts.registered_by,ts.created_at,ts.modified_at,ts.sales_name,tu.mobile_no as salesPhone,tu.status
 	FROM tbl_sales ts
@@ -91,6 +67,32 @@ func (service *salesService) MISDeveloper(request salesRequestDTO.MISDeveloperRe
 	FROM tbl_sales ts
 	JOIN tbl_user tu ON tu.email = ts.sales_email
 	WHERE developer_email = '` + request.EmailDeveloper + `' and date(ts.created_at) >= '` + request.StartDate + `'
+	`
+	} else {
+		sqlStr1 = `SELECT 
+	tu.id,ts.developer_email,ts.sales_email,ts.refferal_code,ts.registered_by,ts.created_at,ts.modified_at,ts.sales_name,tu.mobile_no as salesPhone,tu.status
+	FROM tbl_sales ts
+	JOIN tbl_user tu ON tu.email = ts.sales_email
+	WHERE developer_email = '` + request.EmailDeveloper + `' and ts.developer_email like '%` + request.Keyword + `%'
+	or developer_email = '` + request.EmailDeveloper + `' and ts.refferal_code like '%` + request.Keyword + `%'
+	or developer_email = '` + request.EmailDeveloper + `' and ts.registered_by like '%` + request.Keyword + `%'
+	or developer_email = '` + request.EmailDeveloper + `' and ts.created_at like '%` + request.Keyword + `%'
+	or developer_email = '` + request.EmailDeveloper + `' and ts.modified_at like '%` + request.Keyword + `%'
+	or developer_email = '` + request.EmailDeveloper + `' and ts.sales_name like '%` + request.Keyword + `%'
+	or developer_email = '` + request.EmailDeveloper + `' and tu.mobile_no like '%` + request.Keyword + `%'
+	limit ` + strconv.Itoa(request.Limit) + ` offset ` + strconv.Itoa(request.Offset) + `
+	`
+		sqlStrCountAll1 = `SELECT 
+	count(tu.id)
+	FROM tbl_sales ts
+	JOIN tbl_user tu ON tu.email = ts.sales_email
+	WHERE developer_email = '` + request.EmailDeveloper + `' and ts.developer_email like '%` + request.Keyword + `%'
+	or developer_email = '` + request.EmailDeveloper + `' and ts.refferal_code like '%` + request.Keyword + `%'
+	or developer_email = '` + request.EmailDeveloper + `' and ts.registered_by like '%` + request.Keyword + `%'
+	or developer_email = '` + request.EmailDeveloper + `' and ts.created_at like '%` + request.Keyword + `%'
+	or developer_email = '` + request.EmailDeveloper + `' and ts.modified_at like '%` + request.Keyword + `%'
+	or developer_email = '` + request.EmailDeveloper + `' and ts.sales_name like '%` + request.Keyword + `%'
+	or developer_email = '` + request.EmailDeveloper + `' and tu.mobile_no like '%` + request.Keyword + `%'
 	`
 	}
 
