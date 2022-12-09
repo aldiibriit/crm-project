da715285ef0bac00ca15ee8bfd97e65595db94ad
aldi <aldi.sptra86@gmail.com>
2022-12-09 15:09:04 +0700

    fix urlEncoded and request pengajuanKPR

--- dto/request/KPRRequestDTO/pengajuan-kpr-request.go
+++ dto/request/KPRRequestDTO/pengajuan-kpr-request.go
@@ -1,17 +1,17 @@
 package KPRRequestDTO
 
 type PengajuanKPRRequest struct {
-	NIK                    string  `json:"nik" binding:"required"`
-	SalesID                int     `json:"salesID" binding:"required"`
-	Name                   string  `json:"name" binding:"required"`
-	TanggalLahir           string  `json:"tanggalLahir" binding:"required"`
-	TempatLahir            string  `json:"tempatLahir" binding:"required"`
-	MaritalStatus          string  `json:"maritalStatus" binding:"required"`
-	JenisKelamin           string  `json:"jenisKelamin" binding:"required"`
-	MobileNo               string  `json:"mobileNo" binding:"required"`
-	AlamatKTP              string  `json:"alamatKTP" binding:"required"`
-	AlamatDomisili         string  `json:"alamatDomisili" binding:"required"`
-	KancaTerdekat          string  `json:"kancaTerdekat" binding:"required"`
+	NIK                    string  `json:"nik"`
+	SalesID                int     `json:"salesID"`
+	Name                   string  `json:"name"`
+	TanggalLahir           string  `json:"tanggalLahir"`
+	TempatLahir            string  `json:"tempatLahir"`
+	MaritalStatus          string  `json:"maritalStatus"`
+	JenisKelamin           string  `json:"jenisKelamin"`
+	MobileNo               string  `json:"mobileNo"`
+	AlamatKTP              string  `json:"alamatKTP"`
+	AlamatDomisili         string  `json:"alamatDomisili"`
+	KancaTerdekat          string  `json:"kancaTerdekat"`
 	ApprovalPlafondBrispot float64 `json:"approvalPlafondBrispot"`
 	BiayaAdminNominal      float64 `json:"biayaAdminNominal"`
 	BiayaAdminPercentage   float64 `json:"biayaAdminPercentage"`
